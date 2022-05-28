9c5ca43d3ae19a39c5d018ba7728873e6bcf8bfe
[Adam Ibrahim] <onelordad@gmail.com>
2022-05-28 08:42:08 +0100

    new changes

--- main.py
+++ main.py
@@ -3,9 +3,30 @@
 # find_anagrams("hello", "check") --> False
 # find_anagrams("below", "elbow") --> True
 
-
+# True anagram
 def find_anagram(word, anagram):
     # [assignment] Add your code here
+    word = "hello"
+    anagram = "olleh"
+    str1_anagram = sorted(word)
+    str2_anagram = sorted(anagram)
+    if str1_anagram == str2_anagram:
+        return True
+    else:
+            return False
+print(find_anagram("hello", "olleh"))
 
-    return True
+    
+# False anagram
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    word = "Adam"
+    anagram = "Maddam"
+    str1_anagram = sorted(word)
+    str2_anagram = sorted(anagram)
+    if str1_anagram == str2_anagram:
+        return True
+    else:
+            return False
+print(find_anagram("Adam", "Maddam"))
 
